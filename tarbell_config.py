8e8960b68b82b9adbb3c935ed253cf6d8ee3adbc
David Eads <davideads@gmail.com>
2015-04-14 00:54:51 -0400

    rebuild + destroy

--- tarbell_config.py
+++ tarbell_config.py
@@ -5,10 +5,10 @@ Tarbell project configuration
 """
 
 # Google spreadsheet key
-#SPREADSHEET_KEY = "10HmPJvXi_X2RHb8s_oQ2WGGsBlz-tHuW2n1dsomaD7c"
+SPREADSHEET_KEY = "1F3KjjFzM1cUduyoSxdlLf4TIRbvFZp1dJXJDo0EjtkI"
 
 # Exclude these files from publication
-EXCLUDES = ["*.md", "requirements.txt"]
+EXCLUDES = ["*.md", "requirements.txt", "lib/*", "styles/*"]
 
 # Spreadsheet cache lifetime in seconds. (Default: 4)
 # SPREADSHEET_CACHE_TTL = 4
@@ -19,7 +19,7 @@ EXCLUDES = ["*.md", "requirements.txt"]
 # Get context from a local file or URL. This file can be a CSV or Excel
 # spreadsheet file. Relative, absolute, and remote (http/https) paths can be 
 # used.
-CONTEXT_SOURCE_FILE = "data/_slides.xlsx"
+#CONTEXT_SOURCE_FILE = "data/_slides.xlsx"
 
 # EXPERIMENTAL: Path to a credentials file to authenticate with Google Drive.
 # This is useful for for automated deployment. This option may be replaced by
@@ -32,23 +32,12 @@ S3_BUCKETS = {
      #Provide target -> s3 url pairs, such as:
          #"mytarget": "mys3url.bucket.url/some/path"
      #then use tarbell publish mytarget to publish to it
-    'production': 'recoveredfactory.net/dbootcamp-2015',
-    'staging': 'recoveredfactory.net/dbootcamp-2015',
+    'production': 'recoveredfactory.net/whats-an-internet',
+    'staging': 'recoveredfactory.net/whats-an-internet',
 }
 
 # Default template variables
-DEFAULT_CONTEXT = {
-    'data': [   {   'column1': u'row1, column1',
-                    'column2': u'row1, column2'},
-                {   'column1': u'row2, column1',
-                    'column2': u'row2, column2'}],
-    'headline': u'Test headline',
-    'keyed_data': {   'key1': {   'column1': u'key1, column1',
-                                  'column2': u'key1, column2',
-                                  'key': u'key1'},
-                      'key2': {   'column1': u'key2, column1',
-                                  'column2': u'key2, column2',
-                                  'key': u'key2'}},
-    'name': 'dc-dbootcamp-2015',
-    'title': 'DC Data Bootcamp 2015'
-}
+#DEFAULT_CONTEXT = {
+    #'name': '',
+    #'title': 'DC Data Bootcamp 2015'
+#}
